17272515e257bfe95a12c4fcb32eb260c5cfba11
Naomi <63055040+nkpe@users.noreply.github.com>
2022-06-23 21:27:57 +0100

    addded more js for pair handling - picture appears when cards clicked on

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 "use strict";
 
 // 1. Window.onload - Define images / Text objects to randomise and assign to cards. 
-    // 1a. HTML contains card id 1-'8' and img. JS points to card id & img to remove this. 
+// 1a. HTML contains card id 1-'8' and img. JS points to card id & img to remove this. 
 // 2.  Define Correct Pairs (using objects?)
 // 3. When card clicked, get the image / text id and store in object.
 // 4. When two values recieved, check value of text string matches id of image. 
@@ -12,58 +12,56 @@
 
 // Inital Pairs - Aperol Spritz, Cosmopolitan, Espresso Martini, Pina Colada, 
 
+//Loading Content for Cards
 
-let cards = document.querySelectorAll("div.card");
-let newContent = document.createElement("div");
-
-
-function cardContent(cardPicture , imgID) {
-    this.cardPicture = cardPicture;
-    this.imgID = imgID;
-};
-// console.log(cards);
-
-let AperolImg = new cardContent("cocktail-card-aperol-spritz.png" , "AperolImg");
-
-// console.log(AperolImg);
+window.onload = function () {
+    const aperolImgSrc = "src/imgs/cards/cocktail-card-aperol-spritz.png";
+    const aperolTextSrc = "Aperol Spritz";
 
 
-// Logic for click event
-let cardFlip = (e) => {
-    let cardSelected = e.srcElement.offsetParent;
-    console.log(cardSelected.nodeName);
-    console.log("CardFlip Function is working");
-    console.log(e);
+    //Variable from HTML document
+    let cards = document.querySelectorAll("div.card");
+    // let newContent = document.createElement("div");
 
-    let cardReplace = () => {
+    //Objects to define
+    function cardContent(cardImg, id) {
+        this.cardImg = cardImg;
+        this.id = id;
+    };
 
-        console.log("is background pink?");
+   
+    const aperolImg = new cardContent(aperolImgSrc, 1);
+    const aperolText = new cardContent(aperolTextSrc, 1);
+    //
 
-        let imgBack = cardSelected.querySelector("img");
-        console.log(imgBack);
+    // Logic for click event
+    let cardFlip = (e) => {
+        let cardSelected = e.srcElement.offsetParent;
+        let cardClicked = e.srcElement.offsetParent.id;
+        console.log(cardSelected);
+        console.log(cardClicked);
+        // console.log(e);
 
-        imgBack.replaceWith(newContent);
-           
-            newContent.style.height = "100px";
-            newContent.style.backgroundColor = "pink";
-    }
-    cardReplace();
+        let cardReplace = () => {
 
-};
+            console.log("is Cocktail Image show?");
 
-for (let i=0; i<cards.length; i++){
-    cards[i].onclick = cardFlip;
- 
-};
+            let imgBack = cardSelected.querySelector(".card-img");
+            console.log(imgBack);
 
-//end logic for click event
+            imgBack.src = aperolImg.cardImg;
 
+        }
+        cardReplace();
 
+    };
 
-
+    for (let i = 0; i < cards.length; i++) {
+        cards[i].onclick = cardFlip;
+    };
 
 
-
+    //end logic for click event
 
 
 
@@ -82,31 +80,42 @@ for (let i=0; i<cards.length; i++){
 
 
 
-// let card1Flip = () => {
-//     card1Front.style.display = "block";
-//     card1Back.style.display = "none";
-// };
-
-// card1Back.addEventListener('click' , e =>{
-// card1Flip(e);
-// });
 
 
-// let card2Flip = () => {
-//     card2Front.style.display = "block";
-//     card2Back.style.display = "none";
-// };
-
-// card2Back.addEventListener('click' , e =>{
-// card2Flip(e);
-// });
 
 
-// function cardPair (front1, front2){
-//     this.cardImage = front1;
-//     this.cardTitle = front2;
-// };
 
-// let cardPair1 = new cardPair(card1Back , card2Back);
 
-// console.log(cardPair1);
+
+
+
+    // let card1Flip = () => {
+    //     card1Front.style.display = "block";
+    //     card1Back.style.display = "none";
+    // };
+
+    // card1Back.addEventListener('click' , e =>{
+    // card1Flip(e);
+    // });
+
+
+    // let card2Flip = () => {
+    //     card2Front.style.display = "block";
+    //     card2Back.style.display = "none";
+    // };
+
+    // card2Back.addEventListener('click' , e =>{
+    // card2Flip(e);
+    // });
+
+
+    // function cardPair (front1, front2){
+    //     this.cardImage = front1;
+    //     this.cardTitle = front2;
+    // };
+
+    // let cardPair1 = new cardPair(card1Back , card2Back);
+
+    // console.log(cardPair1);
+}
+
