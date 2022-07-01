77f590ec781353f16133bd6173bd07c1845696ba
Naomi <63055040+nkpe@users.noreply.github.com>
2022-07-01 11:21:45 +0100

    Game working. Issue to sort - user clicks to quick, game doesn't work properly

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const espressoMartiniImgSrc = "src/imgs/cards/cocktail-card-espresso-martini.png
 const espressoMartiniTextSrc = "src/imgs/cards-text/cocktail-cards-text-espresso-martini.png";
 const pinaColadaImgSrc = "src/imgs/cards/cocktail-card-pina-colada.png";
 const pinaColadaTextSrc = "src/imgs/cards-text/cocktail-cards-text-pina-colada.png";
-
+const cardBackImgSrc = "src/imgs/cards/cocktail-card-back.png"
 //Array to store card content
 let cardContentArray = [];
 
@@ -83,43 +83,85 @@ let shuffled = shuffleCardContents(cardContentArray);
 
 
 let clickEvent1Result = null;
+let clickEvent2Result = null;
+let cardClicked1 = null;
+let cardClicked2 = null;
 // Get id of card clicked and match to index from cards array. 
 let cardFlip = (e) => {
+    console.log("START click event function");
     let index = 0;
-    let cardClicked = e.target.id;
+    //store both cards clicked (by element)
+    let cardClicked = e.target;
+    if (cardClicked1 === null){
+        cardClicked1 = e.target;
+    } else {
+        cardClicked2 = e.target;
+    };
 
-    for (let i=0; i<cards.length; i++){
-    //loop through cards until id matched cardClicked
+    for (let i = 0; i < cards.length; i++) {
+        //loop through cards until id matches id of card clicked
         let cardsID = cards[i].id;
-        if (cardsID === cardClicked){
+        if (cardsID === cardClicked.id) {
             index = i;
             // console.log("This is the selected index:", index);
             break;
-        }
+        };
+        cardsID = null;
     };
 
     // console.log("This is event object", e);
 
-    if(clickEvent1Result == null){
+
+    //log first card clicked and compare to second clicked 
+    if (clickEvent1Result === null) {
+        //getting index from object array
         clickEvent1Result = shuffled[index];
+        //replacing img using object src
         e.target.src = clickEvent1Result.cardImg;
-        console.log("1st Click Event", clickEvent1Result);
+        console.log("1st Click Event", cardClicked1 , clickEvent1Result);
+
     } else {
-        let clickEvent2Result = shuffled[index];
+        clickEvent2Result = shuffled[index];
+
         e.target.src = clickEvent2Result.cardImg;
-        console.log("2nd Click Event")
-        if(clickEvent1Result.id === clickEvent2Result.id){
-            console.log("Congrats you've made a match");
-        } else {
-            console.log("This is not a match");
-        }
-        clickEvent1Result = null;
+
+        console.log("2nd Click Event" , cardClicked2 , clickEvent2Result);
+        setTimeout( pairValidate , 2000 );
+        cardClicked = null;
+        //comparing object id's to check for a match
     };
+    console.log("END click event function");
     
-    console.log("Current Index from shuffled", clickEvent1Result);
-    index = 0;
-    // cardsCompare(e);
 };
+
+let pairValidate = () => {
+    console.log("START pair validate function");
+    if (clickEvent2Result !== null && clickEvent1Result.id === clickEvent2Result.id) {
+        console.log("Congrats you've made a match");
+        clickEvent1Result = null;
+        clickEvent2Result = null;
+        cardClicked1 = null;
+        cardClicked2 = null;
+
+    } else if (clickEvent2Result !== null && clickEvent1Result.id !== clickEvent2Result.id) {
+        //show card back again
+        console.log("You've not made a match" , cardClicked1 , cardClicked2);
+        cardClicked1.src = cardBackImgSrc;
+        cardClicked2.src = cardBackImgSrc;
+        clickEvent1Result = null;
+        clickEvent2Result = null;
+        cardClicked1 = null;
+        cardClicked2 = null;
+
+        
+    };
+    console.log("END pair validate function");
+};
+
+
+
+console.log("Current Index from shuffled", clickEvent1Result);
+let index = 0;
 
 
 
@@ -129,7 +171,7 @@ console.log("0 Click Event", clickEvent1Result)
 
 //Store ClickEvent1 & Get Click Event 2
 // let cardsCompare = (e) => {
-    
+
 // };
 
 
@@ -144,6 +186,7 @@ console.log("0 Click Event", clickEvent1Result)
 
 for (let i = 0; i < cards.length; i++) {
     cards[i].onclick = cardFlip;
+
 };  //end logic for click event
 
 
